2fd25f81b0bcdd2932265e4dd287b8ffbcc7e024
Miguel Martinez <duardotrader@gmail.com>
2026-05-22 13:11:25 -0500

    first commit

---
+++ efectos.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function (e) {
+    const ripple = document.createElement("span");
+    ripple.className = "ripple";
+    
+    ripple.style.left = `${e.clientX}px`;
+    ripple.style.top = `${e.clientY}px`;
+
+    document.body.appendChild(ripple);
+
+    setTimeout(() => {
+        ripple.remove();
+    }, 600);
+});
